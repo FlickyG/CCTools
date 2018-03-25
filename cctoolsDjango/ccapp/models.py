248787533b0c572b6ce95476662c48dad15a2817
Adam Green <adam.green@ocado.com>
2018-03-25 20:15:49 +0100

    added 'weapons' to the models file

--- cctoolsDjango/ccapp/models.py
+++ cctoolsDjango/ccapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from django.template.defaultfilters import slugify #section 7.3
+
 # Create your models here.
+
+
+class Weapon():
+    
+    name = models.CharField(max_length = 50)
+    slug = models.SlugField()
+    
+    class Meta:
+        managed = True
+        db_table = 'weapon'
+        app_label = "ccap"
+        
+    def __str__(self):
+        return str(self.name)
+    
+    def save(self, *args, **kwargs):
+        # Uncomment if you don't want the slug to change every time the name changes
+        #if self.id is None:
+        #self.slug = slugify(self.name)
+        self.slug = slugify(self.name) 
+    
